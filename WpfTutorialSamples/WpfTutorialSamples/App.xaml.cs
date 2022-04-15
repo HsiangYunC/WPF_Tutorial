64485acad45d52188fafc04fa051fe27d4ec936f
HsiangYunC <jf33550336@gmail.com>
2022-04-15 16:32:06 +0800

    Command-line parameters in WPF

--- WpfTutorialSamples/WpfTutorialSamples/App.xaml.cs
+++ WpfTutorialSamples/WpfTutorialSamples/App.xaml.cs
@@ -7,15 +7,21 @@ namespace WpfTutorialSamples
     /// </summary>
     public partial class App : Application
     {
-        // manually create starting window
-        private void Application_Startup(object sender, StartupEventArgs e)
-        {
-            // Create the startup window
-            MainWindow wnd = new MainWindow();
-            // Do stuff here, e.g. to the window
-            wnd.Title = "Something else";
-            // Show the window
+		// manually create starting window
+		private void Application_Startup(object sender, StartupEventArgs e)
+		{
+			// Create the startup window
+			MainWindow wnd = new MainWindow();
+
+            // From the Project menu select "[Project name] properties"
+            // and then go to the Debug tab, where you can define a command-line parameter.
+            // c:\Windows\win.ini
+            if (e.Args.Length == 1)
+            {
+                MessageBox.Show("Now opening file: \n\n" + e.Args[0]);
+            }
+
             wnd.Show();
-        }
-    }
+		}
+	}
 }
