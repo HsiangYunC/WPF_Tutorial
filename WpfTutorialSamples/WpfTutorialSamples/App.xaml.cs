d053aa448b7d9b96e76ae2a71b6a2e215d0745ee
HsiangYunC <jf33550336@gmail.com>
2022-04-15 16:11:41 +0800

    App.xaml.cs structure

---
+++ WpfTutorialSamples/WpfTutorialSamples/App.xaml.cs
@@ -0,0 +1,21 @@
+﻿using System.Windows;
+
+namespace WpfTutorialSamples
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+        // manually create starting window
+        private void Application_Startup(object sender, StartupEventArgs e)
+        {
+            // Create the startup window
+            MainWindow wnd = new MainWindow();
+            // Do stuff here, e.g. to the window
+            wnd.Title = "Something else";
+            // Show the window
+            wnd.Show();
+        }
+    }
+}
