60515618dbf839f2b4dcf62c1697f0588a21179f
HsiangYunC <jf33550336@gmail.com>
2022-04-15 13:53:06 +0800

    Basic XAML

---
+++ BasicXAML/BasicXAML/MainWindow.xaml.cs
@@ -0,0 +1,41 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Media;
+
+namespace BasicXAML
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            Button btn = new Button();
+            btn.FontWeight = FontWeights.Bold;
+            btn.VerticalAlignment = VerticalAlignment.Top;
+            btn.Margin = new Thickness(0, 150, 0, 0);
+
+            WrapPanel pnl = new WrapPanel();
+
+            TextBlock txt = new TextBlock();
+            txt.Text = "*Multi*";
+            txt.Foreground = Brushes.Blue;
+            pnl.Children.Add(txt);
+
+            txt = new TextBlock();
+            txt.Text = "*Color*";
+            txt.Foreground = Brushes.Red;
+            pnl.Children.Add(txt);
+
+            txt = new TextBlock();
+            txt.Text = "*Button*";
+            pnl.Children.Add(txt);
+
+            btn.Content = pnl;
+            Grid.Children.Add(btn);
+        }
+    }
+}
