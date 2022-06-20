98e9484adc605327cbb1a8de1f2314608811fbc4
HsiangYunC <jf33550336@gmail.com>
2022-06-20 13:46:06 +0800

    Using the DataContext

--- DataBinding/DataBinding/DataContextSample.xaml.cs
+++ DataBinding/DataBinding/DataContextSample.xaml.cs
@@ -10,29 +10,19 @@ using System.Windows.Documents;
 using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
 using System.Windows.Shapes;
 
 namespace DataBinding
 {
     /// <summary>
-    /// Interaction logic for MainWindow.xaml
+    /// Interaction logic for DataContextSample.xaml
     /// </summary>
-    public partial class MainWindow : Window
+    public partial class DataContextSample : Window
     {
-        public MainWindow()
+        public DataContextSample()
         {
             InitializeComponent();
-        }
-
-        private void HelloBoundWorldSampleButton_Click(object sender, RoutedEventArgs e)
-        {
-            new HelloBoundWorldSample().ShowDialog();
-        }
-
-        private void DataContextSampleButton_Click(object sender, RoutedEventArgs e)
-        {
-            new DataContextSample().ShowDialog();
+            this.DataContext = this;
         }
     }
 }
