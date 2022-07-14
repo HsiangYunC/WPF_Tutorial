555d43880a0ffae416fd65518b5f4d37b0030d4a
JennyChen <jenny.hy.chen@deltaww.com>
2022-07-14 15:23:05 +0800

    Using WPF commands

---
+++ Commands/Commands/UsingCommandsSample.xaml.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace Commands
+{
+    /// <summary>
+    /// UsingCommandsSample.xaml 的互動邏輯
+    /// </summary>
+    public partial class UsingCommandsSample : Window
+    {
+        public UsingCommandsSample()
+        {
+            InitializeComponent();
+        }
+
+        private void NewCommand_Executed(object sender, ExecutedRoutedEventArgs e)
+        {
+            MessageBox.Show("The new command was invoked");
+        }
+
+        private void NewCommand_CanExecute(object sender, CanExecuteRoutedEventArgs e)
+        {
+            e.CanExecute = true;
+        }
+    }
+}
