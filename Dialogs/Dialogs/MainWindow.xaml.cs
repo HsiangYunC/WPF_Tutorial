f092de6c86eb1b7efae67b9034e58d1b5e2ebfdc
Jenny.Chen <jenny.hy.chen@deltaww.com>
2022-07-15 15:42:48 +0800

    The MessageBox

---
+++ Dialogs/Dialogs/MainWindow.xaml.cs
@@ -0,0 +1,20 @@
+﻿using System.Windows;
+
+namespace Dialogs
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void MessageBoxSample_Click(object sender, RoutedEventArgs e)
+        {
+            new MessageBoxSample().ShowDialog();
+        }
+    }
+}
---
+++ Dialogs/Dialogs/MessageBoxSample.xaml.cs
@@ -0,0 +1,72 @@
+﻿using System.Windows;
+
+namespace Dialogs
+{
+    /// <summary>
+    /// MessageBoxSample.xaml 的互動邏輯
+    /// </summary>
+    public partial class MessageBoxSample : Window
+    {
+        public MessageBoxSample()
+        {
+            InitializeComponent();
+        }
+
+        private void btnSimpleMessageBox_Click(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("Hello, world!");
+        }
+
+        private void btnMessageBoxWithTitle_Click(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("Hello, world!", "My App");
+        }
+
+        private void btnMessageBoxWithButtons_Click(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show(
+                "This MessageBox has extra options.\n\nHello, world?",
+                "My App",
+                MessageBoxButton.YesNoCancel);
+        }
+
+        private void btnMessageBoxWithResponse_Click(object sender, RoutedEventArgs e)
+        {
+            MessageBoxResult result = MessageBox.Show(
+                "Would you like to greet the world with a \"Hello, world\"?",
+                "My App",
+                MessageBoxButton.YesNoCancel);
+
+            switch (result)
+            {
+                case MessageBoxResult.Yes:
+                    MessageBox.Show("Hello to you too!", "My App");
+                    break;
+                case MessageBoxResult.No:
+                    MessageBox.Show("Oh well, too bad!", "My App");
+                    break;
+                case MessageBoxResult.Cancel:
+                    MessageBox.Show("Nevermind then...", "My App");
+                    break;
+            }
+        }
+        private void btnMessageBoxWithIcon_Click(object sender, RoutedEventArgs e)
+        {
+            // http://msdn.microsoft.com/en-us/library/system.windows.messageboximage.aspx
+            MessageBox.Show(
+                "Hello, world!",
+                "My App",
+                MessageBoxButton.OK,
+                MessageBoxImage.Information);
+        }
+
+        private void btnMessageBoxWithDefaultChoice_Click(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show(
+                "Hello, world?",
+                "My App", MessageBoxButton.YesNo,
+                MessageBoxImage.Question,
+                MessageBoxResult.No);
+        }
+    }
+}
