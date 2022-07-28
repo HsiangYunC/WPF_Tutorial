e1cf605985d7382642e3db80ba22f68f54c2a896
Jenny.Chen <jenny.hy.chen@deltaww.com>
2022-07-28 17:42:39 +0800

    MenuSample

--- Common_interface_controls/Common_interface_controls/MainWindow.xaml.cs
+++ Common_interface_controls/Common_interface_controls/MainWindow.xaml.cs
@@ -1,17 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
+﻿using System.Windows;
 
 namespace Common_interface_controls
 {
@@ -23,6 +10,11 @@ namespace Common_interface_controls
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        private void MenuSampleButton_Click(object sender, RoutedEventArgs e)
+        {
+            new MenuSample().ShowDialog();
         }
     }
 }
