5ea1c329ad1d940d2f75377367c0309afd116cd5
Jenny.Chen <jenny.hy.chen@deltaww.com>
2022-09-13 09:00:51 +0800

    The FlowDocumentScrollViewer control

---
+++ RichTextControls/RichTextControls/MainWindow.xaml.cs
@@ -0,0 +1,20 @@
+﻿using System.Windows;
+
+namespace RichTextControls
+{
+    /// <summary>
+    /// MainWindow.xaml 的互動邏輯
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void FlowDocumentScrollViewerSampleButton_Click(object sender, RoutedEventArgs e)
+        {
+            new FlowDocumentScrollViewerSample().ShowDialog();
+        }
+    }
+}
